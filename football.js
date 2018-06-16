ca1434c9324fed0691d233b4ea5867da5076e43b
Gerald Bauer <gerald.bauer@gmail.com>
2018-06-16 21:55:38 +0200

    up worldcup 2018

--- football.js
+++ football.js
@@ -1,64 +1,79 @@
 
-var footballdb_widget_new = function( widget_id, api_path_prefix ) {
-  
-  var _api_path_prefix = '';
-  var _$widget;
-  
-  function _init( widget_id, api_path_prefix )
-  {
-    _api_path_prefix = api_path_prefix;
-    _$widget  = $( widget_id );
+
+class FootballDbWidget {
+
+  constructor( widget_id, opts={}) {
+    this.widget_id = widget_id;
+    this.opts      = opts;
+
+    this.$widget =   document.getElementById( widget_id );
+    this.data    =  {}
   }
 
-  function _update( event_key, round_pos )
-  {
-    var api_link = _api_path_prefix + "/event/" + event_key + "/round/" + round_pos + '?callback=?';
-    
-    $.getJSON( api_link, function(json) {
-    
-      var snippet = "";  // build up a hypertext (html) snippet to add/append
-  
-      snippet += "<h3>";
-      snippet += json.event.title;
-      snippet += " - ";
-      snippet += json.round.title;
-      snippet += "</h3>";
-  
-      snippet += "<ul>";
-  
-      $.each( json.games, function( index, game ) {
-        snippet += "<li>";
-        snippet += game.play_at + " | ";
-        snippet += game.team1_title + " (" + game.team1_code +")";
 
-       if( game.score1 != null && game.score2 != null ) {
-         if( game.score1et != null && game.score2et != null ) {
-           if ( game.score1p != null && game.score2p != null ) {
-             snippet += " " + game.score1p + "-" + game.score2p + " pen /";
-           }
-           snippet += " " + game.score1et + "-" + game.score2et + " a.e.t. /";
-         }
-         snippet += " " + game.score1 + "-" + game.score2;
+  fetch( path="2018/worldcup" )
+  {
+    const that = this;
+    const url = "https://raw.githubusercontent.com/openfootball/world-cup.json/master/" + path + ".json";
+    fetch( url )
+    .then( (resp) => resp.json() )
+    .then( function( data ) {
+      console.log( "fetch data:" );
+      console.log( data );
+      that.data = data;
+      that.update_round( 0 );    // note: index starts at zero
+    })
+    .catch(function(err) { console.log(err); });
+  }
+
+  update_round( pos )
+  {
+      console.log( `update_round( ${pos} )` );
+
+      const data = this.data;
+
+      let snippet = "";  // build up a hypertext (html) snippet to add/append
+      snippet += `<h2>${data.name}</h2>`;
+      snippet += "<div>";
+      for( const [idx,round] of data.rounds.entries()) {
+        snippet += `<span id="round${idx+1}" title="${round.name}"> ${idx+1} </span>`;
       }
-      else
-        snippet += " - ";
-      
-      snippet += " " + game.team2_title + " (" + game.team2_code +")";
-      snippet += "</li>";
-    });
+      snippet += "</div>";
+      snippet += `<h3>${data.rounds[pos].name}</h3>`;
 
-    snippet += "</ul>";
-  
-    _$widget.html( snippet );
-    });  // getJSON
-  }  // fn _update
 
-  // call c'tor/constructor
-  _init( widget_id, api_path_prefix );
+      for( const match of data.rounds[pos].matches) {
+        snippet += "<div>";
+        snippet += `#${match.num} | `;
+        snippet += `${match.date} ${match.time}  `;
+        snippet += `${match.team1.name} (${match.team1.code})`;
 
-  // return/export public api
-  return {
-     update: _update
-  }
-  
-} // fn football_widget_new
+        if( match.score1 != null && match.score2 != null ) {
+          if( match.score1et != null && match.score2et != null ) {
+            if( match.score1p != null && match.score2p != null ) {
+              snippet += ` ${match.score1p}-${game.score2p} pen /`;
+            }
+            snippet += ` ${match.score1et}-${match.score2et} a.e.t. /`;
+          }
+          snippet += ` ${match.score1}-${match.score2}`;
+        }
+        else
+          snippet += " vs ";
+
+        snippet += ` ${match.team2.name} (${match.team2.code})`;
+        snippet += ` @ ${match.city} (${match.timezone})`;
+        snippet += "</div>";
+      }
+
+
+      this.$widget.innerHTML = snippet;
+
+      // add onclick handlers
+      for( const [idx,round] of data.rounds.entries()) {
+        // todo/fix: limit scope use this.$widget NOT document
+        const $round = document.getElementById( `round${idx+1}` );
+        $round.addEventListener( "click", ()=>this.update_round( idx ) );
+        // $round.style.color = "red";
+      }
+  }  // fn update
+}
